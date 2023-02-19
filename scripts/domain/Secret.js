ce19e47afab0c5f6ca131b944557ff7c04219009
Heaher S <heatherswope@hotmail.com>
2023-02-19 23:10:00 +0100

    Add Secret JS domain object and test file

---
+++ scripts/domain/Secret.js
@@ -0,0 +1,143 @@
+const { bigNumberIsValid, booleanIsValid, addressIsValid, bytes32IsValid } = require("../util/validations.js");
+
+/**
+ * Domain Entity: Secret
+ *
+ */
+class Secret {
+  /*
+    struct Secret {
+      uint256 id;
+      bytes32 message;
+      uint256 blockNumber;
+      address party1;
+      address party2;
+    }
+  */
+
+  constructor(id, message, blockNumber, party1, party2) {
+    this.id = id;
+    this.message = message;
+    this.blockNumber = blockNumber;
+    this.party1 = party1;
+    this.party2 = party2;
+  }
+
+  /**
+   * Get a new Secret instance from a pojo representation
+   * @param o
+   * @returns {Secret}
+   */
+  static fromObject(o) {
+    const { id, message, blockNumber, party1, party2 } = o;
+    return new Secret(id, message, blockNumber, party1, party2);
+  }
+
+  /**
+   * Get a new Secret instance from a returned struct representation
+   * @param struct
+   * @returns {*}
+   */
+  static fromStruct(struct) {
+    let id, message, blockNumber, party1, party2;
+
+    // destructure struct
+    [id, message, blockNumber, party1, party2] = struct;
+
+    return Secret.fromObject({
+      id: id.toString(),
+      message: message.toString(),
+      blockNumber: blockNumber.toString(),
+      party1,
+      party2,
+    });
+  }
+
+  /**
+   * Get a database representation of this Secret instance
+   * @returns {object}
+   */
+  toObject() {
+    return JSON.parse(this.toString());
+  }
+
+  /**
+   * Get a string representation of this Secret instance
+   * @returns {string}
+   */
+  toString() {
+    return JSON.stringify(this);
+  }
+
+  /**
+   * Get a struct representation of this Secret instance
+   * @returns {string}
+   */
+  toStruct() {
+    return [this.id, this.message, this.blockNumber, this.party1, this.party2];
+  }
+
+  /**
+   * Clone this Secret
+   * @returns {Secret}
+   */
+  clone() {
+    return Secret.fromObject(this.toObject());
+  }
+
+  /**
+   * Is this Secret instance's id field valid?
+   * Must be a string representation of a big number
+   * @returns {boolean}
+   */
+  idIsValid() {
+    return bigNumberIsValid(this.id);
+  }
+
+  /**
+   * Is this Secret instance's message field valid?
+   * Must be a BytesLike
+   * @returns {boolean}
+   */
+  messageIsValid() {
+    return bytes32IsValid(this.message);
+  }
+
+  /**
+   * Is this Secret instance's blockNumber field valid?
+   * Must be a string representation of a big number
+   * @returns {boolean}
+   */
+  blockNumberIsValid() {
+    const { blockNumber } = this;
+    return bigNumberIsValid(blockNumber);
+  }
+
+  /**
+   * Is this Secret instance's party1 field valid?
+   * Must be a eip55 compliant Ethereum address
+   * @returns {boolean}
+   */
+  party1IsValid() {
+    return addressIsValid(this.party1);
+  }
+
+  /**
+   * Is this Secret instance's party2 field valid?
+   * Must be a eip55 compliant Ethereum address
+   * @returns {boolean}
+   */
+    party2IsValid() {
+      return addressIsValid(this.party2);
+    }
+  /**
+   * Is this Secret instance valid?
+   * @returns {boolean}
+   */
+  isValid() {
+    return this.idIsValid() && this.messageIsValid() && this.blockNumberIsValid() && this.party1IsValid() && this.party2IsValid();
+  }
+}
+
+// Export
+module.exports = Secret;
---
+++ scripts/util/validations.js
@@ -0,0 +1,115 @@
+const hre = require("hardhat");
+const ethers = hre.ethers;
+const eip55 = require("eip55");
+
+/**
+ * Must be a string representation of a big number
+ * @param bigNumber
+ * @param options
+ * - {boolean} optional
+ * - {number} gt - greater than
+ * - {number} lte - less than or equal to
+ * - {boolean} empty - empty string is valid
+ * @returns {boolean}
+ */
+function bigNumberIsValid(bigNumber, { optional, gt, lte, empty } = {}) {
+  let valid = false;
+  if (optional && (bigNumber == undefined || bigNumber == null)) {
+    return true;
+  }
+  try {
+    valid =
+      typeof bigNumber === "string" &&
+      (empty
+        ? bigNumber === "" || typeof ethers.BigNumber.from(bigNumber) === "object"
+        : typeof ethers.BigNumber.from(bigNumber) === "object");
+    if (gt != undefined) {
+      valid = valid && ethers.BigNumber.from(bigNumber).gt(gt);
+    }
+    if (lte != undefined) {
+      valid = valid && ethers.BigNumber.from(bigNumber).lte(lte);
+    }
+  } catch (e) {}
+  return valid;
+}
+
+/**
+ * Must be a array of big numbers
+ * @param bigNumberArray
+ * @returns {boolean}
+ */
+function bigNumberArrayIsValid(bigNumberArray) {
+  let valid = false;
+  try {
+    valid =
+      Array.isArray(bigNumberArray) &&
+      bigNumberArray.reduce((previousValue, currentValue) => previousValue && bigNumberIsValid(currentValue), true);
+  } catch (e) {}
+  return valid;
+}
+
+/**
+ * Must be a number belonging to the enumTypes array
+ * @params {number} enumValue
+ * @params {Array<numbers>} enumTypes - array of numbers
+ * @returns {boolean}
+ */
+function enumIsValid(enumValue, enumTypes) {
+  let valid = false;
+  try {
+    valid = enumTypes.includes(enumValue);
+  } catch (e) {}
+  return valid;
+}
+
+/**
+ * Must be a eip55 compliant Ethereum address
+ * @returns {boolean}
+ */
+function addressIsValid(address) {
+  let valid = false;
+  try {
+    valid = eip55.verify(eip55.encode(address));
+  } catch (e) {}
+  return valid;
+}
+
+/**
+ * Must be a boolean
+ * @returns {boolean}
+ */
+function booleanIsValid(boolean) {
+  let valid = false;
+  try {
+    valid = typeof boolean === "boolean";
+  } catch (e) {}
+  return valid;
+}
+
+/**
+ * Must be a string
+ * @returns {boolean}
+ */
+function stringIsValid(string) {
+  let valid = false;
+  try {
+    valid = typeof string === "string";
+  } catch (e) {}
+  return valid;
+}
+
+function bytes32IsValid(bytes32) {
+  let valid = false;
+  try {
+    valid = ethers.utils.isBytesLike(bytes32);
+  } catch (e) {}
+  return valid;
+}
+
+exports.bigNumberIsValid = bigNumberIsValid;
+exports.enumIsValid = enumIsValid;
+exports.addressIsValid = addressIsValid;
+exports.booleanIsValid = booleanIsValid;
+exports.bigNumberArrayIsValid = bigNumberArrayIsValid;
+exports.stringIsValid = stringIsValid;
+exports.bytes32IsValid = bytes32IsValid;
